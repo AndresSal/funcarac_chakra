434e40e441dbd4f4b7bc76813b9401f6aa1d1e0c
AndresSal <andres.salazar01@epn.edu.ec>
2020-01-20 16:36:54 -0500

    some other changes

--- src/gameObjects/library/pages/picturePage.js
+++ src/gameObjects/library/pages/picturePage.js
@@ -18,7 +18,7 @@ export default class PicturePage extends BasePage{
 
     setContent(scene){
         this.picture = scene.add.image(0,0,'ejemploCuento');
-        let taleStoryBox = new TaleStoryBox(scene,0,0,1,10);
+        let taleStoryBox = new TaleStoryBox(scene,0,0,this.indexPage,this.totalPage);
         this.contentGroup.add(this.picture);
         this.contentGroup.add(taleStoryBox);
 
@@ -31,44 +31,5 @@ export default class PicturePage extends BasePage{
             y:this.page.height/6-this.page.height/7
         });
         this.addGroupContent(scene);
-    }
-
-    createContainer(scene,phrase,innerText,cellW,cellH){
-        let mainText = scene.add.text(0,0,phrase,
-        {fontFamily:'Helvetica',
-         fontSize:'36px',
-         color:'#f5ec2f'});
-        let window = scene.add.image(0,0,'ventanaIndice');
-        let indexText = scene.add.text(-15,-20,innerText,
-        {fontFamily:'Helvetica',
-         fontSize:'40px',
-         color:'#000'}); 
-        let indexContainer = scene.add.container(0,0,[window,indexText]);
-        indexContainer.setSize(window.width,window.height);
-
-        let elementsGroup = scene.add.group();
-        elementsGroup.add(mainText);
-        elementsGroup.add(indexContainer);
-
-        Phaser.Actions.GridAlign(elementsGroup.getChildren(),{
-            width:2,
-            height:1,
-            cellWidth:cellW,
-            cellHeight:cellH,
-            x:0,
-            y:0
-        });
-
-        let container = scene.add.container(0,0);
-        
-        elementsGroup.getChildren().forEach((el)=>{
-            container.add(el);
-        });
-
-        container.setSize(mainText.width+indexContainer.width,indexContainer.height);
-        scene.add.existing(container);
-
-        return container;
-    }
-    
+    }    
 }
