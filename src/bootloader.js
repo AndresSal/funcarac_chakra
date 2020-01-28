8103b3ee5f4b2847f92f0dd312b0102842255919
AndresSal <andres.salazar01@epn.edu.ec>
2020-01-28 16:56:43 -0500

    change bootloader organization

--- src/bootloader.js
+++ src/bootloader.js
@@ -1,5 +1,6 @@
 import { DEFAULT_WIDTH, DEFAULT_HEIGHT, AtlasInfo } from "./consts/mainuiLib.js";
 import { talePictures } from "./data/tales/talesInformation.js";
+import { andenResources, chakrasResources,chakrasControlPanelResources, totalResources } from "./data/loaderData.js";
 
 class BootLoader extends Phaser.Scene{
     constructor(){
@@ -46,69 +47,28 @@ class BootLoader extends Phaser.Scene{
         });
         assetText.setOrigin(0.5,0.5);
 
-        //tools
-        // this.load.image('cajaHtas','./assets/herramientas/tools_box.png');
-        // this.load.image('placaHtas','./assets/herramientas/plaque.png');
-        // this.load.image('bordeHta','./assets/herramientas/tool_board.png');
-        // this.load.image('cuerpoHta','./assets/herramientas/tool_body.png');
-        // this.load.image('etiquetaHta','./assets/herramientas/tool_title.png');
-        // this.load.image('abono','./assets/herramientas/abono.png');
-        // this.load.image('agua','./assets/herramientas/agua.png');
-        // this.load.image('azadon','./assets/herramientas/azadon.png');
-        // this.load.image('chisguete','./assets/herramientas/chisguete.png');
-        // this.load.image('pala','./assets/herramientas/pala.png');
-        // this.load.image('tijeras','./assets/herramientas/tijeras.png');
-
-        //seeds
-        // this.load.image('cajaSemillas','./assets/semillas/seeds_box.png');
-        // this.load.image('placaSemillas','./assets/semillas/box_plaque.png');
-        // this.load.image('cajaIntSemillas','./assets/semillas/inner_box.png');
-        // this.load.image('bordeSemilla','./assets/semillas/seed_board.png');
-        // this.load.image('cuerpoSemilla','./assets/semillas/seed_body.png');
-        // this.load.image('canasta','./assets/semillas/seed_basket.png');
-        // this.load.image('canastaBloqueada','./assets/semillas/unlocked_seed.png');
-        // this.load.image('etiquetaSemilla','./assets/semillas/seed_label.png');
-
-        //anden
-        this.load.image('anden','./assets/anden/platform.png');
-        this.load.image('placaAnden','./assets/anden/platform_plaque.png');
-        this.load.image('bordeChakra','./assets/anden/chakra_board.png');
-        
-        
-        //chakras
-        this.load.image('chakraNatural','./assets/chakras/grassed_chakra.png');
-        this.load.image('chakraLimpiada','./assets/chakras/cleaned_chakra.png');
-        this.load.image('chakraAbonada','./assets/chakras/fertilized_chakra.png');
-        this.load.image('chakraSurcada','./assets/chakras/lined_chakra.png');
-
-        //maleza
-        this.load.image('maleza','./assets/malezas/maleza.png');
-
-        //panel control
-        this.load.image('chakraPanel','./assets/anden/chakra_panel.png');
-        this.load.image('planta','./assets/anden/chakra.png');
-        this.load.image('icono_reloj','./assets/anden/clock.png');
-        this.load.image('icono_abono','./assets/anden/compost.png');
-        this.load.image('icono_agua','./assets/anden/water.png');
-        this.load.image('contenedor_barra','./assets/anden/status_bar_body.png');
-        this.load.image('barra_estado','./assets/anden/status_bar.png');
+        totalResources.forEach((resourcePack)=>{
+            resourcePack.forEach((resource)=>{
+                this.load.image(resource.key,resource.path);
+            })
+        })
 
         //carac
-        this.load.image('cajaPiezas','./assets/carac/piecesBox.png');
-        this.load.image('placaPiezas','./assets/carac/piecesPlaque.png');
-        this.load.image('cajaTablero','./assets/carac/boardBox.png');
-        this.load.image('placaTablero','./assets/carac/boardPlaque.png');
-        this.load.image('flechaPiezas','./assets/carac/arrow.png');
-        this.load.image('piezaRoja','./assets/carac/pieceRed.png');
-        this.load.image('piezaAmarilla','./assets/carac/pieceYellow.png');
-        this.load.image('piezaVerde','./assets/carac/pieceGreen.png');
-        this.load.image('piezaAzul','./assets/carac/pieceBlue.png');
-        this.load.image('fondoRojo','./assets/carac/backgroundRed.png');
-        this.load.image('fondoAmarillo','./assets/carac/backgroundYellow.png');
-        this.load.image('fondoVerde','./assets/carac/backgroundGreen.png');
-        this.load.image('fondoAzul','./assets/carac/backgroundBlue.png');
-        this.load.image('ranura','./assets/carac/slotBody.png');
-        this.load.image('etiqueta_pieza','./assets/carac/pieceLabel.png');
+        // this.load.image('cajaPiezas','./assets/carac/piecesBox.png');
+        // this.load.image('placaPiezas','./assets/carac/piecesPlaque.png');
+        // this.load.image('cajaTablero','./assets/carac/boardBox.png');
+        // this.load.image('placaTablero','./assets/carac/boardPlaque.png');
+        // this.load.image('flechaPiezas','./assets/carac/arrow.png');
+        // this.load.image('piezaRoja','./assets/carac/pieceRed.png');
+        // this.load.image('piezaAmarilla','./assets/carac/pieceYellow.png');
+        // this.load.image('piezaVerde','./assets/carac/pieceGreen.png');
+        // this.load.image('piezaAzul','./assets/carac/pieceBlue.png');
+        // this.load.image('fondoRojo','./assets/carac/backgroundRed.png');
+        // this.load.image('fondoAmarillo','./assets/carac/backgroundYellow.png');
+        // this.load.image('fondoVerde','./assets/carac/backgroundGreen.png');
+        // this.load.image('fondoAzul','./assets/carac/backgroundBlue.png');
+        // this.load.image('ranura','./assets/carac/slotBody.png');
+        // this.load.image('etiqueta_pieza','./assets/carac/pieceLabel.png');
 
         //library
         this.load.image('libreria','./assets/library/library.png');
